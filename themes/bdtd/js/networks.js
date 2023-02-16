4576dae9072af3598b2935b87f0288ce7203ec5e
Jesiel Viana <jesielviana@proton.me>
2023-02-16 13:33:03 -0300

    add conteúdo das  páginas do menu institucional

--- themes/bdtd/js/networks.js
+++ themes/bdtd/js/networks.js
@@ -6,11 +6,11 @@ async function getAllNetworks() {
     const response = await axios.get(`${REMOTE_API_URL}/networks`);
     hideLoader();
     const networks = response.data;
-    console.log('networks', networks)
+    console.log("networks", networks);
     return networks;
   } catch (errors) {
     hideLoader();
-    showMessageError('#networks-page');
+    showMessageError("#networks-page");
     console.error(errors);
   }
 }
@@ -24,22 +24,18 @@ function fillDatanetworks(networks) {
   });
   const options = {
     valueNames: [
-      'name',
-      'institution',
-      'validSize',
-      { data: ['id'] },
-      { attr: 'href', name: 'link' },
+      "name",
+      "institution",
+      "validSize",
+      { data: ["id"] },
+      { attr: "href", name: "link" },
     ],
 
     // Since there are no elements in the list, this will be used as template.
     item: `<li class="network-item">
-      <h3><a href="" class="link name"></a></h3>
-      <p><b>${getTranslatedText(
-        'Instituição responsável'
-      )}</b>: <span class="institution"></span></p>
-      <p><b>${getTranslatedText(
-        'Número de documentos coletados'
-      )}</b>: <span class="validSize"></span></p>
+      <h3><a href="" class="link name text-secondary"></a></h3>
+      <p><b>Instituição responsável</b>: <span class="institution"></span></p>
+      <p><b>Número de documentos coletados</b>: <span class="validSize"></span></p>
       </li>`,
     page: 10,
     pagination: [
@@ -49,23 +45,23 @@ function fillDatanetworks(networks) {
     ],
   };
 
-  networksList = new List('networks', options, networks);
+  networksList = new List("networks", options, networks);
 }
 
 function sortDatanetworks() {
-  const sortSelectElement = document.querySelector('#sort-select');
-  let sortOrder = 'asc';
-  sortSelectElement.addEventListener('change', (e) => {
+  const sortSelectElement = document.querySelector("#sort-select");
+  let sortOrder = "asc";
+  sortSelectElement.addEventListener("change", (e) => {
     if (sortOrder != sortSelectElement.value) {
-      networksList.sort('name', { order: sortSelectElement.value });
+      networksList.sort("name", { order: sortSelectElement.value });
       sortOrder = sortSelectElement.value;
     }
   });
 }
 
 function watchingUpdateOnList() {
-  const list = document.querySelector('.list');
-  networksList.on('updated', (element) => {
+  const list = document.querySelector(".list");
+  networksList.on("updated", (element) => {
     list.style.counterReset = `item ${element.i - 1}`;
   });
 }
@@ -86,11 +82,11 @@ async function getIndicatorsByDocumentType() {
 }
 
 function fillIndicatorsByDocumentType(indicators) {
-  const sidebarElement = document.querySelector('#side-collapse-format');
+  const sidebarElement = document.querySelector("#side-collapse-format");
   indicators.forEach((indicator) => {
     const item = `<a onclick="filterNetworks('${
       indicator.name
-    }')"  class="facet js-facet-item facetAND">
+    }')"  class="text-secondary facet js-facet-item facetAND">
     <span class="text">
       <span class="facet-value">${getTranslatedText(indicator.name)}</span>
     </span>
@@ -122,9 +118,9 @@ function filterNetworks(filter) {
 }
 
 function exportsCSV(networks) {
-  const btnExport = document.querySelector('.btn-export-csv');
-  btnExport.addEventListener('click', () => {
-    let csvContent = 'data:text/csv;charset=utf-8,';
+  const btnExport = document.querySelector(".btn-export-csv");
+  btnExport.addEventListener("click", () => {
+    let csvContent = "data:text/csv;charset=utf-8,";
 
     const jsonObject = networksList.filtered
       ? JSON.stringify(networksList.matchingItems.map((i) => i._values))
@@ -138,51 +134,51 @@ function exportsCSV(networks) {
 }
 
 function showTotalFind(total) {
-  const totalLabel = document.querySelector('.networks-label');
-  totalLabel.innerHTML = `${getTranslatedText('Retornaram')} ${formatNumber(
+  const totalLabel = document.querySelector(".networks-label");
+  totalLabel.innerHTML = `${getTranslatedText("Retornaram")} ${formatNumber(
     total
-  )} ${getTranslatedText('fontes')}`;
+  )} instituições encontradas`;
 }
 
 function showTotal(total) {
-  const badgeTotal = document.querySelector('.badge-total');
+  const badgeTotal = document.querySelector(".badge-total");
   badgeTotal.innerHTML = formatNumber(total);
   showTotalFind(total);
 }
 
 function ConvertToCSV(objArray) {
-  const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
-  let csv = 'Nome,Instituição,Tipo de fonte,URL,Email,ISSN,Quantidade de itens';
-  csv += '\r\n';
+  const array = typeof objArray !== "object" ? JSON.parse(objArray) : objArray;
+  let csv = "Nome,Instituição,Tipo de fonte,URL,Email,ISSN,Quantidade de itens";
+  csv += "\r\n";
   array.forEach((item) => {
     let line =
       `"${item.name}"` +
-      ',' +
+      "," +
       `"${item.institution}"` +
-      ',' +
+      "," +
       item.sourceType +
-      ',' +
+      "," +
       item.sourceUrl +
-      ',' +
+      "," +
       `"${item.email}"` +
-      ',' +
+      "," +
       item.issn +
-      ',' +
+      "," +
       item.validSize;
-    line = line.replaceAll('#', '%23');
-    csv += line + '\r\n';
+    line = line.replaceAll("#", "%23");
+    csv += line + "\r\n";
   });
   return csv;
 }
 
 function listenerListAllNetworks() {
-  const listAll = document.querySelector('#list-all');
-  listAll.addEventListener('click', () => {
+  const listAll = document.querySelector("#list-all");
+  listAll.addEventListener("click", () => {
     filterNetworks();
   });
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const allNetworks = await getAllNetworks();
   const indicators = await getIndicatorsByDocumentType();
   fillIndicatorsByDocumentType(indicators);
