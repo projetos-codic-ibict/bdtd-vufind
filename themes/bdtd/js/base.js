2b6062c68921477eec724414d428c543a99e5b63
Jesiel Viana <jesielviana@proton.me>
2023-11-09 17:27:12 -0300

    ajustando link da api do oaisbr e ils

--- themes/bdtd/js/base.js
+++ themes/bdtd/js/base.js
@@ -2,7 +2,15 @@ const INDICATORS_FACETS =
   'search?type=AllFields&page=0&limit=0&sort=relevance&facet[]=author_facet&facet[]=dc.subject.por.fl_str_mv&facet[]=eu_rights_str_mv&facet[]=dc.publisher.program.fl_str_mv&facet[]=dc.subject.cnpq.fl_str_mv&facet[]=publishDate&facet[]=language&facet[]=format&facet[]=institution&facet[]=dc.contributor.advisor1.fl_str_mv';
 
 let API_BASE_URL;
-let REMOTE_API_URL = 'http://172.16.16.112:3000/api/v1';
+let REMOTE_API_URL = 'https://api-oasisbr.ibict.br/api/v1';
+
+if (window.location.hostname === 'bdtd.ibict.br') {
+  API_BASE_URL = `https://${window.location.host}/bdtd/api/v1`;
+  REMOTE_API_URL = `https://api-oasisbr.ibict.br/api/v1`;
+} else {
+  API_BASE_URL = `http://${window.location.host}/bdtd/api/v1`;
+  REMOTE_API_URL = `http://${window.location.host}:3000/api/v1`;
+}
 
 let loader = '';
 
@@ -24,14 +32,6 @@ function showMessageError(element) {
     ${getTranslatedText('Failed to load data sources, please try again later')}
     </div>`;
   }
-}
-
-if (window.location.hostname === 'oasisbr.ibict.br') {
-  API_BASE_URL = `https://${window.location.host}/bdtd/api/v1`;
-  REMOTE_API_URL = `https://api-${window.location.host}/api/v1`;
-} else {
-  API_BASE_URL = `http://${window.location.host}/bdtd/api/v1`;
-  REMOTE_API_URL = `http://${window.location.host}:3000/api/v1`;
 }
 
 async function getIndicatorsBy(filter) {
@@ -64,7 +64,6 @@ async function getIndicatorsFromVufindApi(lookfor, type) {
     console.error(errors);
   }
 }
-
 
 document.addEventListener('DOMContentLoaded', async () => {
   loader = document.querySelector('.loader ');
